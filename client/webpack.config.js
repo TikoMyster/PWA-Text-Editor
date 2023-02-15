377eb1fca891c61218f46aee464214e64e83ee05
TikoMyster <tikomyster20@gmail.com>
2023-02-14 17:08:21 -0800

    updated webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "Jate"
+      }),
+      
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Jate",
+        short_name: 'Text Editor',
+        description: 'Text Editor for offline/ online use',
+        background_color: '#0099ff',
+        theme_color: '#0099ff',
+        start_url: './',
+        publicPath: './',
+        icons: [{
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+      }],
+
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       
     ],
 
     module: {
-      rules: [
+      rules: [{
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/transform-runtime", "@babel/plugin-proposal-object-reset-spread"],
+          },
+        },
+      },
         
       ],
     },
